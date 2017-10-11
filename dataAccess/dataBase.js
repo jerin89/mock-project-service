68e47f99a24254061a4e26e78f05221662eea9b8
jerin89 <jerin.john2@cognizant.com>
2017-10-11 16:42:44 +0530

    api added to list company data

---
+++ dataAccess/dataBase.js
@@ -0,0 +1,7 @@
+
+const pg = require('pg');
+require('dotenv').config();
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+const client = new pg.Client(connectionString);
+client.connect();
+module.exports = client;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const client = require('../dataAccess/dataBase');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/GetList', (req, res, next) => {
+  let results = []; 
+  // Get a Postgres client from the connection pool 
+  client.query("SELECT * FROM company", function (err, result) {
+    if (err) {
+      console.log(err)
+    }
+    result.rows.forEach(row=>{
+      results.push(row)
+    });
+   res.json(results);
+  })
 });
 
 module.exports = router;
